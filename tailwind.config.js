8e62136b3115957b2bc0d7e8deda04b107b855d5
Higor Denomar <higordenomar@gmail.com>
2024-02-23 18:58:13 -0300

    chore: add gray-600 color class

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       colors: {
         primary: '#32C0C6',
         gray: {
+          700: '#4E4E4E',
           600: '#616161',
           500: '#8c8c8c',
           200: '#E3E6E9',
