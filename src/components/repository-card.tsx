ee7221fcaa03db058c709629d05e877f819e85ca
Higor Denomar <higordenomar@gmail.com>
2024-02-23 20:45:04 -0300

    feat: makes the repository card have dynamic data

--- src/components/repository-card.tsx
+++ src/components/repository-card.tsx
@@ -1,28 +1,44 @@
 import { HiOutlineHeart } from 'react-icons/hi'
-import { cls } from '../lib/utils'
+import { cls, formatDate } from '../lib/utils'
+import type { Repository } from '../types/github'
 
 type RepositoryCardProps = {
-  repository?: unknown
+  repository: Repository
 }
 
 export function RepositoryCard({ repository }: RepositoryCardProps) {
-
-  console.log(repository)
   return (
     <li className='flex items-start gap-4 border rounded p-4'>
       <div className='w-full'>
-        <h3 className='font-semibold text-lg'>Pokepiker</h3>
+        <a
+          href={repository.html_url}
+          target='_blank'
+          className='font-semibold text-lg'
+        >
+          {repository.name}
+        </a>
 
-        <p className='text-gray-500 text-sm mt-1 mb-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis animi minima nam voluptatibus illum et voluptatum nemo sint rem dolor quibusdam vel, maiores eveniet aliquid magni odio blanditiis architecto veritatis.</p>
+        {repository.description ? (
+          <p className='text-gray-500 text-sm mt-1'>{repository.description}</p>
+        ) : null}
 
-        <div className='flex gap-6 text-xs'>
-          <span>Typescript</span>
-          <data>Updated on 17 Apr 2021</data>
+        <div className='flex gap-6 mt-4 text-xs'>
+          {repository.language ? (
+            <span className='tech' datatype={repository.language}>{repository.language}</span>
+          ) : null}
+
+          <span>
+            Updated on
+            {' '}
+            <time className='capitalize' dateTime={repository.updated_at}>
+              {formatDate(repository.updated_at)}
+            </time>
+          </span>
         </div>
       </div>
 
       <button type='button' className={cls(
-        'border bg-gray-200 rounded-full min-h-10 min-w-10 flex items-center justify-center', {
+        'border bg-gray-100 rounded-full min-h-10 min-w-10 flex items-center justify-center', {
         'border-primary text-primary bg-transparent': false
       })}>
         <HiOutlineHeart
