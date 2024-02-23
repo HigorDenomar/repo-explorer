c45b256782c0f6864276a6d35ffbbdf3aaf0c89d
Higor Denomar <higordenomar@gmail.com>
2024-02-23 20:43:07 -0300

    feat: creates utility to format date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from 'tailwind-merge'
 export function cls(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Updated on 16 Jul 2020
+export function formatDate(date: string) {
+  return new Date(date)
+    .toLocaleDateString('pt-BR', {
+      dateStyle: 'medium',
+    })
+    .replace(/()\.|de\b/g, '')
+    .split(' ')
+    .filter(Boolean)
+    .join(' ')
+}
