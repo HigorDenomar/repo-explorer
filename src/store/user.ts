0166e3f017190ec6a4fb9bc99947123a39bc2a6a
Higor Denomar <higordenomar@gmail.com>
2024-02-24 18:53:05 -0300

    feat: add isLoading to the user store

--- src/store/user.ts
+++ src/store/user.ts
@@ -3,8 +3,10 @@ import type { User } from '../types/github'
 
 type StoreProps = {
   user?: User
+  isLoading?: boolean
 }
 
 export const useUserStore = create<StoreProps>(() => ({
   user: undefined,
+  isLoading: false,
 }))
