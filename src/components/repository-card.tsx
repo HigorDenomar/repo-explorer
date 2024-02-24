83813f9fa01e454d13589d1bcb26e3be2771bf46
Higor Denomar <higordenomar@gmail.com>
2024-02-23 22:34:01 -0300

    feat: integrate repository-card with the store

--- src/components/repository-card.tsx
+++ src/components/repository-card.tsx
@@ -1,5 +1,6 @@
 import { HiOutlineHeart } from 'react-icons/hi'
 import { cls, formatDate } from '../lib/utils'
+import { useFavoriteStore } from '../store/favorite'
 import type { Repository } from '../types/github'
 
 type RepositoryCardProps = {
@@ -7,6 +8,19 @@ type RepositoryCardProps = {
 }
 
 export function RepositoryCard({ repository }: RepositoryCardProps) {
+  const { favorites, add, remove } = useFavoriteStore()
+
+  const isFavorite = !!favorites.find(favorite => favorite.id === repository.id)
+
+  function handleToFavoriteRepository() {
+    if (isFavorite) {
+      remove(repository.id)
+      return
+    }
+
+    add(repository)
+  }
+
   return (
     <li className='flex items-start gap-4 border rounded p-4'>
       <div className='w-full'>
@@ -37,13 +51,17 @@ export function RepositoryCard({ repository }: RepositoryCardProps) {
         </div>
       </div>
 
-      <button type='button' className={cls(
-        'border bg-gray-100 rounded-full min-h-10 min-w-10 flex items-center justify-center', {
-        'border-primary text-primary bg-transparent': false
-      })}>
+      <button
+        type='button'
+        className={cls(
+          'border bg-gray-100 rounded-full min-h-10 min-w-10 flex items-center justify-center', {
+          'border-primary text-primary bg-transparent': isFavorite
+        })}
+        onClick={handleToFavoriteRepository}
+      >
         <HiOutlineHeart
           size={18}
-          className={cls({ 'fill-primary': false })}
+          className={cls({ 'fill-primary': isFavorite })}
         />
       </button>
     </li>
