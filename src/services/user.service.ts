0d40959072593599a923eefd127a148762829843
Higor Denomar <higordenomar@gmail.com>
2024-02-24 18:52:36 -0300

    feat: add user type to the service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,8 @@
 import { api } from '../lib/api'
+import type { User } from '../types/github'
 
 export const UserService = {
   get: async (username: string) => {
-    return await api.get(`/users/${username}`)
+    return await api.get<User>(`/users/${username}`)
   },
 }
