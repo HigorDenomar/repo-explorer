4c3d12f8727106f2ea4c1b18f0fcb5b11562316e
Higor Denomar <higordenomar@gmail.com>
2024-02-25 10:40:22 -0300

    feat: prepares the repository service to handle paging

--- src/services/repository.service.ts
+++ src/services/repository.service.ts
@@ -1,8 +1,25 @@
 import { api } from '../lib/api'
 import type { Repository } from '../types/github'
 
+type GetRepositoryServiceParams = {
+  username: string
+  page?: number
+  per_page?: number
+}
+
 export const RepositoryService = {
-  get: async (username: string) => {
-    return await api.get<Repository[]>(`/users/${username}/repos`)
+  get: async ({
+    username,
+    page = 1,
+    per_page = 10,
+  }: GetRepositoryServiceParams) => {
+    await new Promise((resolve) => setTimeout(resolve, 500))
+    return await api.get<Repository[]>(`/users/${username}/repos`, {
+      params: {
+        page,
+        per_page,
+        sort: 'updated',
+      },
+    })
   },
 }
