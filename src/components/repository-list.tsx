696b339e0e5574a9f7c91278555d339d6fb1596b
Higor Denomar <higordenomar@gmail.com>
2024-02-25 11:26:49 -0300

    feat: uses react-query's useInfiniteQuery
    necessary to create pagination logic and make infinite scrolling

--- src/components/repository-list.tsx
+++ src/components/repository-list.tsx
@@ -1,28 +1,43 @@
-import { useQuery } from 'react-query';
+import { useInfiniteQuery } from 'react-query';
 
+import { useMemo } from 'react';
 import { RepositoryService } from '../services/repository.service';
-import { useRepositoryStore } from '../store/repository';
 import { useUserStore } from '../store/user';
+import { InfiniteScroll } from './infinite-scroll';
 import { RepositoryCard } from './repository-card';
 import { RepositoryListSkeleton } from './skeletons/repository-list-skeleton';
 
 export function RepositoryList() {
-  const { repositories } = useRepositoryStore()
   const { user } = useUserStore()
 
-  const { isLoading } = useQuery({
+  const {
+    data,
+    isLoading,
+    isFetched,
+    isFetching,
+    hasNextPage,
+    fetchNextPage
+  } = useInfiniteQuery({
     queryKey: ['repositories', user?.login],
-    queryFn: async () => {
-      const { data } = await RepositoryService.get(user?.login as string)
+    queryFn: async ({ pageParam }) => {
+      const { data } = await RepositoryService.get({
+        username: user?.login as string,
+        page: pageParam
+      })
       return data
     },
-    onSuccess(data) {
-      useRepositoryStore.setState({ repositories: data ?? [] })
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length ? allPages.length + 1 : undefined
     },
     enabled: !!user?.login,
+    refetchOnWindowFocus: false
   })
 
-  if (isLoading || !user?.id) {
+  const repositories = useMemo(() => {
+    return data?.pages.reduce((acc, page) => [...acc, ...page], [])
+  }, [data])
+
+  if ((isLoading && isFetched) || !user?.id) {
     return <RepositoryListSkeleton />
   }
 
@@ -30,11 +45,17 @@ export function RepositoryList() {
     <article className='col-span-1 w-full mt-10 md:mt-0'>
       <h2 className='text-primary font-semibold text-xl mb-4 text-center md:text-start'>Repositórios</h2>
 
-      <ul className='flex flex-col gap-4'>
-        {repositories.map((repository) => (
-          <RepositoryCard key={repository.id} repository={repository} />
-        ))}
-      </ul>
+      <InfiniteScroll
+        fetchNextPage={fetchNextPage}
+        hasNextPage={hasNextPage}
+        isLoading={isFetching}
+      >
+        <ul className='flex flex-col gap-4'>
+          {repositories?.map((repository) => (
+            <RepositoryCard key={repository.id} repository={repository} />
+          ))}
+        </ul>
+      </InfiniteScroll>
     </article>
   )
 }
