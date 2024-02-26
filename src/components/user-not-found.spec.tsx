40b8d4ecd40d261d1ad5f163e866b9b2cb3cfbf8
Higor Denomar <higordenomar@gmail.com>
2024-02-26 13:11:20 -0300

    test: add user-not-found test suite

---
+++ src/components/user-not-found.spec.tsx
@@ -0,0 +1,30 @@
+import { faker } from '@faker-js/faker'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { UserNotFound } from './user-not-found'
+
+const term = faker.internet.userName()
+
+describe('UserNotFound Component', () => {
+  beforeEach(() => {
+    render(<UserNotFound term={term} />)
+  })
+
+  it('should render the not found term', () => {
+    expect(screen.getByText(`"${term}"`)).toBeInTheDocument()
+  })
+
+  it('should render the title', () => {
+    expect(screen.getByText(/nenhum usuário encontrado/i)).toBeInTheDocument()
+  })
+
+  it('should render the description', () => {
+    expect(screen.getByText(/verifique se a escrita está correta ou tente novamente/i)).toBeInTheDocument()
+  })
+
+  it('should render the image', () => {
+    expect(screen.getByRole('img')).toBeInTheDocument()
+  })
+})
