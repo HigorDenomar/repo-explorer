aed022e8a0c13130251d17949385ba05596429ae
Higor Denomar <higordenomar@gmail.com>
2024-03-04 16:09:36 -0300

    feat: creates list users service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,11 @@
 import { api } from '../lib/api'
-import type { User } from '../types/github'
+import type { ListUsers, User } from '../types/github'
 
 export const UserService = {
   get: async (username: string) => {
     return await api.get<User>(`/users/${username}`)
+  },
+  list: async (search: string) => {
+    return await api.get<ListUsers>(`/search/users?q=${search}&per_page=5`)
   },
 }
